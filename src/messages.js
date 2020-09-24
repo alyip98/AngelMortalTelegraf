b5be001aa8a2790d7f05007485e3f9913679e20d
alyip98 <alyip98@gmail.com>
2020-09-24 14:10:43 +0800

    Support custom bot prefix

--- src/messages.js
+++ src/messages.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+const prefix = process.env.PREFIX || "anm";
+
 module.exports = {
     RegisterReminder: 'Please key in your 9 digit code to register!',
     NoGroupChats: "Please don't add me to groups! Byeeee 👋",
     BotWelcome: (name, chatTarget) => `Welcome${name}! This bot will act as a messaging platform between you and your ${chatTarget}. Simply send a message here and we will pass it to your ${chatTarget}`,
-    ReferToBot: (chatAs) => `Please go to the ${chatAs}-bot at @anm_${chatAs.toLowerCase()}_bot to start chatting with your ${chatAs} as well`,
+    ReferToBot: (chatAs) => `Please go to the ${chatAs}-bot at @${prefix}_${chatAs.toLowerCase()}_bot to start chatting with your ${chatAs} as well`,
     RegisterWelcome: "Paste the 9 digit code sent to you here to register",
     UnregisteredTarget: (chatTarget) => `It seems that your ${chatTarget} hasn't registered with the bot on Telegram, we can't deliver your message to them. Don't worry, we'll let you know as soon as they have registered!`,
     DeregisterSuccess: "Successfully deregistered",
@@ -14,4 +17,4 @@ module.exports = {
     HelpMessage: "This bot allows you to communicate with your angel/mortal anonymously.\nRegister with the code given to you by typing\n`/register <code>`\nOnce registered, you can send messages to your angel/mortal just by sending them here just like any other Telegram chat. Currently supported message types are text, stickers, photos and videos.",
     RegisteredNotifier: (chatTarget) => `Your ${chatTarget} has registered with the bot on Telegram. Happy chatting!`,
     NotRegistered: "Not registered",
-}
+};
