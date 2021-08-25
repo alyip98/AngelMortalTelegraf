7c9d1302f8cae31fa6e579dc9d69212fa79220f0
Alfred <alyip98@gmail.com>
2021-08-25 14:10:57 +0800

    Add shuffle

--- load-data.js
+++ load-data.js
@@ -33,14 +33,28 @@ async function main() {
             results.push(newPerson);
         })
         .on('end', () => {
-            const msg = results.map(p => p.getIntroForMortal())
+
+            /* Randomize array in-place using Durstenfeld shuffle algorithm */
+            function shuffleArray(array) {
+                for (var i = array.length - 1; i > 0; i--) {
+                    var j = Math.floor(Math.random() * (i + 1));
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            shuffleArray(results)
+
+            const msg = results.map(p => p.name)
+
+
             console.log(msg);
 
-            const bot = new Telegraf(process.env.DEBUG_BOT_TOKEN);
-
-            msg.slice(1,4).forEach(m => {
-                bot.telegram.sendMessage(process.env.ME, m, {"parse_mode": "HTML"})
-            })
+            // const bot = new Telegraf(process.env.DEBUG_BOT_TOKEN);
+            //
+            // msg.slice(1,4).forEach(m => {
+            //     bot.telegram.sendMessage(process.env.ME, m, {"parse_mode": "HTML"})
+            // })
 
             results.forEach(p => model.addPerson(p))
             model.generateUuids()
