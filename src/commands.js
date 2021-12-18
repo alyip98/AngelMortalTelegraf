e1f82fb5a5a4acbe050ae177af950656f8a3079e
gnahzeus <zhangyue1689@gmail.com>
2021-12-18 16:39:13 +0800

    resolve len

--- src/commands.js
+++ src/commands.js
@@ -110,8 +110,8 @@ PhotoHandler = async (ctx) => {
     const caption = ctx.message.caption || ""
     const target = ctx.isAngel ? ctx.angel : ctx.mortal
     if (target.isRegistered()) {
-        const len = ctx.message.photo.length - 1
-        const fileLink = await ctx.telegram.getFileLink(photos[len].file_id)
+        const len = ctx.message.photo.length
+        const fileLink = await ctx.telegram.getFileLink(photos[len - 1].file_id)
         await ctx.otherBot.telegram.sendPhoto(target.telegramId, {url: fileLink}, {caption})
     } else {
         await ctx.reply(messages.UnregisteredTarget(ctx.chatTarget))
